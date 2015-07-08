bf7f2803608996113dc37cd74853a4b9753c2c8b
Matt Carter <m@ttcarter.com>
2015-07-08 10:50:02 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+var _ = { // Selected Lodash functions
+	isArray: require('lodash.isarray'),
+	isObject: require('lodash.isobject'),
+	isRegExp: require('lodash.isregexp'),
+};
+
+var options = {
+	// Array of RegExps or functions to run on each field in a return object to deside if it should be output
+	removeFields: [/^_/],
+};
+
+/**
+* Walker function to iterate though the collection / array / object tree
+* @param mixed obj The object to iterate over
+* @return Either the incomming object or the rewritten object without any bad fields
+*/
+var walker = function(obj) {
+	if (_.isArray(obj)) {
+		for (var i = 0; i < obj.length; i++) {
+			obj[i] = walker(obj[i]);
+		}
+	} else if (_.isObject(obj)) {
+		var newObj = {};
+		for (var k in obj) {
+			if (!obj.hasOwnProperty(k)) continue;
+			var include = true;
+			for (var rf in options.removeFields) {
+				if (_.isRegExp(options.removeFields[rf])) {
+					if (options.removeFields[rf].test(k)) include = false;
+				} else { // Assume its a function
+					if (options.removeFields[rf].test(v, k)) include = false;
+				}
+				if (!include) break;
+			}
+
+			if (include) newObj[k] = walker(obj[k]);
+		}
+		obj = newObj;
+	}
+	return obj;
+};
+
+module.exports = function(userSettings) {
+	// Adopt user setting overrides if any {{{
+	if (userSettings)
+		for (var k in userSettings)
+			options[k] = userSettings[k];
+	// }}}
+
+	return function(res, result) {
+		res.send(walker(result));
+	};
+};
